25411bf99a97ff6dfb145c832304d591106ce0de
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-04 17:51:21 -0700

    base commit - lists, conditions, loop, sets

---
+++ sequences.py
@@ -0,0 +1,17 @@
+# name = "Harry"
+
+# print(name[0])
+
+# lists -> sequence of mutable values
+# names = ["Harry", "Ron", "Hermione"]
+
+# print(names[0])
+
+
+# tuple -> sequence of immutable values
+# coordinate = (10.0, 20.0)
+
+# set -> collection of unique values
+
+
+# dict -> collection of key-value pairs
---
+++ sets.py
@@ -0,0 +1,19 @@
+# Create an empty set
+s = set()
+
+# Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+print(s)
+
+# Remove an element with value 2 from set
+s.remove(2)
+print(s)
+
+
+# Len method -> gets length
+print(f"The set has {len(s)} elements")
