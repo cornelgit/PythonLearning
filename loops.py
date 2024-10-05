2098f59b31108c5c75555c6514f6b9386f741b8f
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-04 17:54:50 -0700

    finished loops

--- loops.py
+++ loops.py
@@ -1,5 +1,17 @@
-# For loop
+# For loop - list
 for i in [0, 1,2,3,4,5]:
     print(i)
 
+
+# For loop - list
+names = ["Harry", "Ron", "Hermione"]
+for name in names:
+    print(name)
+
+
+
+# For loop - sequence of chars - string
+name = "Harry"
+for character in name:
+    print(character)
 
