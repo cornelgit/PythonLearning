8829de97629299a2485ef3ab9e3c64c99a4b7997
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-04 18:17:40 -0700

    finished functions and importing modules

---
+++ functions.py
@@ -0,0 +1,4 @@
+# Define a function - will square a number
+def square(x):
+    return x * x
+
---
+++ squares.py
@@ -0,0 +1,6 @@
+from functions import square # imports
+
+# Defines a square function
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+
