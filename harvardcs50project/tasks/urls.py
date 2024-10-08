caf66f29c73a1b8533612f519476ffff90dc9e93
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-08 06:02:51 -0700

    Added add.html file and updated urlpatterns in tasks sub app

--- harvardcs50project/tasks/urls.py
+++ harvardcs50project/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path("", views.index, name="index")
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add", views.add, name="add")
 ]
--- harvardcs50project/tasks/views.py
+++ harvardcs50project/tasks/views.py
@@ -8,6 +8,10 @@ tasks = [
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index/html", {
+    return render(request, "tasks/index.html", {
         "tasks": tasks
     })
+
+
+def add(request):
+    return render(request, "tasks/add.html")
