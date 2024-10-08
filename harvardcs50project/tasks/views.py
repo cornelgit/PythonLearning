50ed40dea8c9c2c173c4d0707cbd4e9f96fbbb33
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-08 05:47:20 -0700

    Implemented urls and views for tasks sub app

--- harvardcs50project/tasks/views.py
+++ harvardcs50project/tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+tasks = [
+    "Foo",
+    "Bar",
+    "Baz"
+]
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index/html", {
+        "tasks": tasks
+    })
