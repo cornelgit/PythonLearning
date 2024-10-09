792d106ade32a38d0e3b46766ef06d91793803e2
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-08 19:21:20 -0700

    Added form validation for add task

--- harvardcs50project/tasks/views.py
+++ harvardcs50project/tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = [
     "Foo",
@@ -7,11 +8,27 @@ tasks = [
 ]
 
 # Create your views here.
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
 
-
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["tasks"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
+
+
