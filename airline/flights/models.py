2299d58c0b064e565df75689759b5b797157d8ea
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-11 00:28:13 -0700

    Implemented string representation for Flight class model

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -6,3 +6,7 @@ class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
     duration = models.IntegerField()
+
+    # return a string representation of this object
+    def __str__(self):
+        return (f"{self.id}: {self.origin} to {self.destination}")
