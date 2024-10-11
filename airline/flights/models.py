ff256dad4a38ef488bdfca68649af99c5409381d
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-11 00:12:37 -0700

    Applied migrations for flights subapp

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# every model will be a Python class
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
